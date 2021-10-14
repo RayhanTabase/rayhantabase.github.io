5f333e4f1cc6672625413a35f7c132bb7125b2f0
Rayhan <nofussdufus@gmail.com>
2021-10-14 18:33:31 +0000

    added different styling and reshaped items to look like model piece

--- js/page.js
+++ js/page.js
@@ -14,10 +14,20 @@ function showContent(section){
     if(section == "home"){
         document.querySelector("#home-content").style.display = "block";
         document.querySelector("#conatct").style.opacity = 0;
+        document.querySelector("body").style.background = "rgb(222, 100, 73)";
     }
-    if(section == "about_me")  document.querySelector("#about-content").style.display = "block";
-    if(section == "skills")  document.querySelector("#skills-content").style.display = "block";
-    if(section == "portfolio")  document.querySelector("#portfolio-content").style.display = "block";
+    if(section == "about_me"){
+        document.querySelector("#about-content").style.display = "block";
+        document.querySelector("body").style.background = "#8D6A9F";
+    } 
+    if(section == "skills"){
+        document.querySelector("#skills-content").style.display = "block";
+        document.querySelector("body").style.background = "#577590";
+    } 
+    if(section == "portfolio"){
+        document.querySelector("#portfolio-content").style.display = "block";
+        document.querySelector("body").style.background = "#087F8C";
+    }  
 }
 
 function showContact(){

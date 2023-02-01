b77f625562de817d8be7aa84f42472d762326fb2
Rayhan <maouslayer@gmail.com>
2023-02-01 14:44:48 +0000

    Updates

---
+++ precache-manifest.881431ce87ac2eccd7292a0a3b2e7ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b434f5a0bcf4127495",
+    "url": "/static/css/main.ab30c863.chunk.css"
+  },
+  {
+    "revision": "76b434f5a0bcf4127495",
+    "url": "/static/js/main.76b434f5.chunk.js"
+  },
+  {
+    "revision": "6b96eb06600376dd9db0",
+    "url": "/static/js/1.6b96eb06.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fa9aa1fa33860ab25b306784f9b0230",
+    "url": "/static/media/linkedin.7fa9aa1f.svg"
+  },
+  {
+    "revision": "554f5c9fa476786be242d99901c435dd",
+    "url": "/static/media/github.554f5c9f.svg"
+  },
+  {
+    "revision": "4152a3edea4343edc3064b4795fd04ea",
+    "url": "/index.html"
+  }
+];

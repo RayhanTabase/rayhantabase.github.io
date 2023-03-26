14eef7d12f865a557a40a57c35708bd4ce589cce
Salim Abdulai <maouslayer@gmail.com>
2023-03-26 21:10:31 +0000

    Fix minor grammar error
    Fix minor grammar error

--- src/Components/About/Description.js
+++ src/Components/About/Description.js
@@ -6,7 +6,7 @@ const Description =() => {
        For a few years now have gained experience working with multiple technologies and frameworks 
        such as Python, Django, HTML, CSS,JavaScript, React, Ruby and Rails. 
        I have built everything from from landing pages, games, mobile applications, to APIs.
-       I find myslef always excited when working towards solving a problem irrespective of the type of application.
+       I find myself always excited when working towards solving a problem irrespective of the type of application.
     </div>
   );
 }
